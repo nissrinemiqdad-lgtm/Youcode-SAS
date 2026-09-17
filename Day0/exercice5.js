170592cde61049cb7f70afdc676d69432ae4472e
nissrinemiqdad-lgtm <nissrinemiqdad@gmail.com>
2026-09-17 15:13:11 +0100

    correction exercice5

--- Day0/exercice5.js
+++ Day0/exercice5.js
@@ -5,5 +5,5 @@ if (num1>num2 && num1>num3)
     console.log("le plus grand des trois nombres est "+ num1 );
 else if (num2>num1 && num2>num3)
     console.log("le plus grand des trois nombres est " + num2);
-else if (num3>num1 && num3>num2)
-    console.log("le plus grand des trois nombres est " + num3 )
+else
+    console.log("le plus grand des trois nombres est " + num3 );
--- Day0/exercice8.js
+++ Day0/exercice8.js
@@ -1,7 +1,7 @@
 let angle1 = 90
 let angle2 = 45
 let angle3 = 45
-if ( angle1 + angle2 + angle3 ===180 && angle1>0 && angle2>0 && angle3>0)
+if ( angle1 + angle2 + angle3 ==180 && angle1>0 && angle2>0 && angle3>0)
     console.log("triangle valid");
 else 
     console.log("triangle non valid")
