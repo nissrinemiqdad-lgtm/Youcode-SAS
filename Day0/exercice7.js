2f0e6dd2a9baecd50303e3a9881dd25b2efde352
nissrinemiqdad-lgtm <nissrinemiqdad@gmail.com>
2026-09-17 15:31:33 +0100

    exercice7

---
+++ Day0/exercice7.js
@@ -0,0 +1,25 @@
+let num1 = 12
+let num2 = 1
+let operator;
+
+switch(operator){
+    case "+":
+        console.log(num1 + num2);
+        break;
+    case "-":
+        console.log(num1 - num2);
+        break;
+    case "*":
+        console.log(num1*num2);
+        break;
+    case "/":
+        if(num2==0)
+            console.log("ERROR");
+        else
+            console.log(num1/num2);
+        break;
+
+}
+
+
+
