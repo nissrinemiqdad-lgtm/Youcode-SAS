eb571ad4e91f61a6649886d04706a4dc70f6ca72
nissrinemiqdad-lgtm <nissrinemiqdad@gmail.com>
2026-09-17 14:35:59 +0100

    exercice5

---
+++ Day0/exercice5.js
@@ -0,0 +1,9 @@
+let num1=5
+let num2=6 
+let num3=7
+if (num1>=num2 && num1>=num3)
+    console.log("le plus grand des trois nombres est "+ num1 );
+else if (num2>=num1 && num2>=num3)
+    console.log("le plus grand des trois nombres est " + num2);
+else if (num3>=num1 && num3>=num2)
+    console.log("le plus grand des trois nombres est " + num3 )
