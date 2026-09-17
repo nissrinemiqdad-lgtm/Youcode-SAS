d1dbf2a5e65cb39f0d8d87523a8534b0fc863dfd
nissrinemiqdad-lgtm <nissrinemiqdad@gmail.com>
2026-09-17 14:02:19 +0100

    exercice4

--- Day0/exercice4.js
+++ Day0/exercice4.js
@@ -1,11 +1,13 @@
-let score = 65
+let score = 0
 if (score>=90 && score<=100)
      console.log("A");
 else if (score>=80 && score<=89)
     console.log("B");
 else if (score>=70 && score<=79)
     console.log("C");
+else if (score<70 && score>=0)
+    console.log("F")
 else 
-    console.log("F");
+    console.log("score is out our limit")
 
 
