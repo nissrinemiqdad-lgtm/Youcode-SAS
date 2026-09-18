96dc4c96ea4f5d922c5966f43d4b247e6eba9a8a
nissrinemiqdad-lgtm <nissrinemiqdad@gmail.com>
2026-09-18 16:28:11 +0100

    exercice5

---
+++ Day1/exercice5.js
@@ -0,0 +1,7 @@
+let text = "Bonjour mes amies ";
+let count = 0;
+for(let letter of text ){
+    if(letter === "a" || letter === "e" || letter === "o" || letter === "i" || letter === "u"){
+        count++; 
+    }}
+    console.log(count);
